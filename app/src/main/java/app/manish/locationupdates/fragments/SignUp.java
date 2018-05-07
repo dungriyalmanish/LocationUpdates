0cdd76bb308d15f95312d0c4524c594447de6c0b
Manish Dungriyal <dungriyalmanish@gmail.com>
2018-05-07 14:01:00 +0530

    initial files
    basic files update

---
+++ app/src/main/java/app/manish/locationupdates/fragments/SignUp.java
@@ -0,0 +1,30 @@
+package app.manish.locationupdates.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.manish.locationupdates.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SignUp extends Fragment {
+
+
+    public SignUp() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+
+}
